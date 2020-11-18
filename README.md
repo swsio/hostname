2bea5d4098fe93498703c2a9e2d56406b927d562
Florian Fröhlich <florian@Florians-MBP.localdomain>
2020-11-18 19:29:24 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Hostname
+PHP Script to display the Hostname of a pod
+
+## Includes bench.php to test webserver performance
---
+++ index.php
@@ -0,0 +1,32 @@
+<html>
+	<body>
+                Diese Seite läuft auf POD: <b>
+<?php
+$hostname = gethostname();
+print $hostname;
+		$secondsWait = 0.5;
+header("Refresh:$secondsWait");
+
+		
+
+if (strpos($hostname, 'blue') !== false) {
+echo "<body style='background-color:blue'>";
+};
+		
+if (strpos($hostname, 'green') !== false) {
+echo "<body style='background-color:green'>";
+};
+
+#if (strpos($hostname, 'demo') !== false) {
+#echo "<body style='background-color:yellow'>";
+#};
+		
+?>
+
+
+</b>
+		
+<p><b>Variante 1</b></p>
+	</body>
+</html>
+
